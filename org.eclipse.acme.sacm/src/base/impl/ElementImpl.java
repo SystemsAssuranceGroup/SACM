ab5fdf8f5f057d7596df2aef9a34e1b67c2e41a3
Ran Wei <will@Rans-MacBook-Pro.local>
2020-08-30 16:52:27 +0800

    Structured Assurance Case Metamodel conforming to v2.1

---
+++ org.eclipse.acme.sacm/src/base/impl/ElementImpl.java
@@ -0,0 +1,39 @@
+/**
+ */
+package base.impl;
+
+import base.Base_Package;
+import base.Element;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Element</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public abstract class ElementImpl extends MinimalEObjectImpl.Container implements Element {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ElementImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return Base_Package.Literals.ELEMENT;
+	}
+
+} //ElementImpl
